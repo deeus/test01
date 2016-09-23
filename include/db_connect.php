95978456c8793b5b21cb5b04c01a56a963a8da17
deeus <danaska28@gmail.com>
2016-09-23 17:23:24 +0500

    Update db_connect.php

--- include/db_connect.php
+++ include/db_connect.php
@@ -11,4 +11,3 @@ mysql_query ("set_client='utf8'");
 mysql_query ("set character_set_results='utf8'");
 mysql_query ("set collation_connection='utf8_general_ci'");
 mysql_query ("SET NAMES utf8");
-?>
